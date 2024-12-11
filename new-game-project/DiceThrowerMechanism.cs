3f739af3cdb802a7ed573d782072d066b4715c60
Jonathan Miroshnik <fistuk2009@gmail.com>
2024-12-11 17:57:16 +0200

    made multi-dice throw

--- new-game-project/DiceThrowerMechanism.cs
+++ new-game-project/DiceThrowerMechanism.cs
@@ -2,66 +2,127 @@ using Godot;
 using System;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Allows to throw dice and get their values after their tumbling
+/// </summary>
 public partial class DiceThrowerMechanism : Node
 {
+	// Die to be thrown
 	[Export]
-	public Dice dice;
+	public PackedScene dice;
 
-	public bool stopped = false;
+	[Export]
+	// The number of Dice we throw  // TODO: make it always >= 1
+	public int numDice = 1;
 
+	[Export]
+	// The position where the Dice are spawned from
+	public Node3D diceSpawnPos;
+
+	// Instantiated Dice
+	private Dice[] instDice; 
+
+	// Time given to the dice to stop tumbling
 	private int MAX_MILLISECS_TO_STOP = 5000;
 
-	// do Ready
+	// Value of a die to be considered a victorious dice
+	private int MIN_WINNING_DICE = 4;
+
+
 	public override async void _Ready() {
-		ResetDice();
-		var final = await ThrowDice();
+        ResetDice();
+		int final = await ThrowDice();
 
 		GD.Print("FINAL " + final.ToString());
 	}
 
+	// Deletes the currently existing dice
+	private void DeleteDice() {
+		if (instDice == null) return;
+
+		// for loop over instDice deleting each one
+		for (int i = 0; i < instDice.Length; i++)
+		{
+			instDice[i].QueueFree();
+		}
+
+		instDice = null;
+	}
+	
+	// Recreates/resets the current dice
 	private void ResetDice() {
-		// TODO: maybe stopped is also unnecessary?
-		stopped = false;
-	}
-
-	private void PhysicallyThrowToMiddle() {
-		Random curRand = new Random();
-		dice.ApplyForce(new Vector3(curRand.NextInt64(0, 30), curRand.NextInt64(0, 30), curRand.NextInt64(0, 30)));
-		dice.ApplyTorqueImpulse(new Vector3(curRand.NextInt64(0, 30), curRand.NextInt64(0, 30), curRand.NextInt64(0, 30)));
-		return;
-	}
-
-	public async Task<int> ThrowDice() {
-		PhysicallyThrowToMiddle();
-
-		Vector3 checkPos = dice.GlobalPosition;
+		DeleteDice();
 		
+		// Creating the dice
+		instDice = new Dice[numDice];
+		for (int i = 0; i < numDice; i++)
+		{
+			instDice[i] = dice.Instantiate<Dice>();
+			instDice[i].GlobalPosition = diceSpawnPos.GlobalPosition;
+			AddChild(instDice[i]);
+		}
+	}
+
+	// Throws a given die
+	private void PhysicallyThrowToMiddle(RigidBody3D curDice) {
+		Random curRand = new Random();
+
+		curDice.ApplyForce(new Vector3(curRand.NextInt64(0, 30), curRand.NextInt64(0, 30), curRand.NextInt64(0, 30)));
+		curDice.ApplyTorqueImpulse(new Vector3(curRand.NextInt64(0, 30), curRand.NextInt64(0, 30), curRand.NextInt64(0, 30)));
+	}
+
+	// Throws dice and gets the number of victourious dice	
+	public async Task<int> ThrowDice() {
+		if (instDice == null) return -1;
+
+		// Throwing the dice randomly
+		for (int i = 0; i < instDice.Length; i++)
+		{
+			if (instDice[i] == null) return -1;
+			PhysicallyThrowToMiddle(instDice[i]);
+		}
+
+		Vector3 checkPos;
 		int milliseconds = MAX_MILLISECS_TO_STOP;
 		int MILLISECS_REDUCTION_EACH_CHECK = 100;
 
-		while(milliseconds > 0) {
-			await Task.Delay(MILLISECS_REDUCTION_EACH_CHECK);
+		// Waiting for the dice to stop tumbling
+		for (int i = 0; i < instDice.Length; i++)
+		{
+			checkPos = instDice[i].GlobalPosition;
 
-			if (checkPos == dice.GlobalPosition) {
+			if (milliseconds <= 0) {
 				break;
 			}
-			else {
-				checkPos = dice.GlobalPosition;
+
+			while(milliseconds > 0) {
+				await Task.Delay(MILLISECS_REDUCTION_EACH_CHECK);
+
+				if (checkPos == instDice[i].GlobalPosition) {
+					break;
+				}
+				else {
+					checkPos = instDice[i].GlobalPosition;
+				}
+
+				milliseconds -= MILLISECS_REDUCTION_EACH_CHECK;
 			}
-
-			milliseconds -= MILLISECS_REDUCTION_EACH_CHECK;
 		}
 
-		// TODO: maybe stopped is also unnecessary?
-		stopped = true;
-
-		if (stopped) {
-			int topSide = dice.WhichSideUp();
-			ResetDice();
-			return topSide;
+		// Finding how many of the dice are victorious
+		int num_of_victor_dice = 0;
+		for (int i = 0; i < instDice.Length; i++)
+		{
+			int curSide = instDice[i].WhichSideUp();
+			if (curSide >= MIN_WINNING_DICE) {
+				num_of_victor_dice++;
+			}
 		}
-		
-		// TODO: maybe stopped is also unnecessary? as above
-		return 0;
+
+		DeleteDice();
+
+		return num_of_victor_dice;
 	}
+
+	// TODO: need non-victorious die throwing mechanism, for e.x. D20
 }
